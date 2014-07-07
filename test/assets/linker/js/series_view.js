c12e643488cfd34980bcca96ea9c65ff9b23ab65
Anthony Cameron <ar.cameron@gmail.com>
2014-07-06 23:15:36 -0400

    cleanup, small bug in slideshow

--- test/assets/linker/js/series_view.js
+++ test/assets/linker/js/series_view.js
@@ -1,6 +1,6 @@
 var startSimpleSlideshow = function (el) {
 	var $parent = $(el).find(".thumbnails");
-	console.log($parent, $parent[0])
+	clearInterval(window.slideshow);
 	window.slideshow = setInterval(function () {
 		var $shown = $parent.find(".video-thumbnail:not(.hidden)");
 		var $next = $shown.next();
@@ -18,21 +18,15 @@ var stopSimpleSlideshow = function () {
 
 $(document).on("ready", function () {
 	$(document).on("focus", ".video", function (e) {
-
 		startSimpleSlideshow(e.currentTarget);
-		console.log("focused", e);
 	});
 	$(document).on("blur", ".video", function (e) {
-		console.log("blur", e);
 		stopSimpleSlideshow();
 	});
 	$(document).on("mouseenter", ".video", function (e) {
-		console.log(e.target, e.currentTarget);
 		startSimpleSlideshow(e.currentTarget);
-		console.log("mouseenter", e);
 	});
 	$(document).on("mouseleave", ".video", function (e) {
-		console.log("mouseleave", e);
 		stopSimpleSlideshow();
 	});
 });
